29ce9ee7912d5dc078fe881551115cc4f958e47a
ZuzannaZawartka <raf.zuzia7@gmail.com>
2025-02-03 17:59:15 +0100

    feat: added dockerfile

---
+++ swift-data-api/src/main/java/com/example/swiftdataapi/SwiftDataApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.swiftdataapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftDataApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwiftDataApiApplication.class, args);
+    }
+
+}
---
+++ swift-data-api/src/test/java/com/example/swiftdataapi/SwiftDataApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swiftdataapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftDataApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
